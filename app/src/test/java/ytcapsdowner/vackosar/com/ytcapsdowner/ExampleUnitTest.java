8b8d462387c06ee3e65f11cc011a87ca5051868b
vackosar <vackosar@users.noreply.github.com>
2017-01-10 08:42:27 +0100

    Test down caps

--- app/src/test/java/ytcapsdowner/vackosar/com/ytcapsdowner/ExampleUnitTest.java
+++ app/src/test/java/ytcapsdowner/vackosar/com/ytcapsdowner/ExampleUnitTest.java
@@ -2,6 +2,15 @@ package ytcapsdowner.vackosar.com.ytcapsdowner;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLDecoder;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 /**
@@ -12,6 +21,31 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        String videoInfo = convertStreamToString(new URL("http://www.youtube.com/get_video_info?video_id=6Mfw_LUwo08").openConnection().getInputStream());
+        String captionTracks = extractTokenValue("caption_tracks", videoInfo);
+        String url = extractTokenValue("u", captionTracks);
+        String caps = convertStreamToString(new URL(url).openConnection().getInputStream());
+//        connection.getInputStream()
+        // http://www.youtube.com/get_video_info?video_id=6Mfw_LUwo08
+        // caption_tracks=
+        // decode
+        // u=
+        // decode
+        // get
+    }
+
+    private String extractTokenValue(String name, String tokens) throws UnsupportedEncodingException {
+        for (String token: tokens.split("&")) {
+            if (token.startsWith(name + "=")) {
+                return URLDecoder.decode(token.split("=")[1], "UTF-8");
+            }
+        }
+        throw new RuntimeException("Token not found");
+    }
+    static String convertStreamToString(java.io.InputStream is) throws IOException {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        String output = s.hasNext() ? s.next() : "";
+        is.close();
+        return output;
     }
 }
